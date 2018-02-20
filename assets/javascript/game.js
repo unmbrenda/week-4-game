cc3348efb36627c73bc5e3b4687bc04a6f7fe74d
unmbrenda <brenda.l.bassett@gmail.com>
2018-02-19 19:45:01 -0600

    more stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,19 +3,28 @@ var targetBarbieIndex = -1;
 $(".topChoices").on("click", ".card", function () {
     var originals = $(".topChoices").children(".card");
     var targetBarbie = getBarbieName(this);
-    for (let i = 0; i < originals.length; i++) {
+    for (var i = 0; i < originals.length; i++) {
         if (getBarbieName(originals[i]) === targetBarbie) {
             targetBarbieIndex = i;
             break;
         }
-
     }
-    
     var yourSelection = $(".yourChoice");
     var yourSelectionKids = $(yourSelection).children(".card");
     var yourSelectionBarbie = yourSelectionKids[targetBarbieIndex];
     $(yourSelectionBarbie).show();
 
+
+    var enemySelection = $(".enemiesAvailable");
+    var enemySelectionKids = $(enemySelection).children(".card");
+    for (var i = 0; i < enemySelectionKids.length; i++) {
+        if (i != targetBarbieIndex) {
+            var frenemy = enemySelectionKids[i];
+            $(frenemy).show();
+        }
+    }
+    $(".topChoices").hide();
+    $("#choose").hide();
 });
 
 function getBarbieName(barbie) {
